4685411c4a545882b88b1a75638d24846d9a9bf4
Alex Weisberger <alex.weisberger@viewthespace.com>
2020-04-17 19:44:41 -0400

    Initial commit

---
+++ src/Freecell.bs.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var React = require("react");
+
+function Freecell(Props) {
+  return React.createElement("div", undefined, "Testing");
+}
+
+var make = Freecell;
+
+exports.make = make;
+/* react Not a pure module */
